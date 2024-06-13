01e2f99c6a9109bf11f05d20bc0db7fa822877b8
Devin de Spindler <devin.vipxxx@gmail.com>
2024-06-13 15:35:12 +0200

    express imported and ESLint added & rules defined

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const express = require('express');
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Server läuft auf localhost:3000");
+});
+
+
+
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/index.html");
+});
+
