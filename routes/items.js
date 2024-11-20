bfe5c027c56e62a82a9610dd476c26ca245cb1e4
Devin de Spindler <151910900+De27vin@users.noreply.github.com>
2024-11-20 10:13:17 +0100

    Metanet version (#10)
    * v1.0
    
    With server connections and .htaccess file to show that the login.html is the index-file.
    
    * v1.0.1
    
    Minor changes of the paths in register.html and login.html.
    
    * v1.0.2
    
    Change of forms in register.html and login.html.
    
    * add: Prepare HTML for mysql connection

--- routes/items.js
+++ routes/items.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
-const pool = require("../database");
 
 router.use(express.json());
 router.use(bodyParser.json());
