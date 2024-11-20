5b917b4e09439ea92727b9893da4c6453f500c97
Devin de Spindler <devin.vipxxx@gmail.com>
2024-11-20 09:54:07 +0100

    add: Prepare HTML for mysql connection

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const pool = require("../database");
 
 router.post("/register", async (request, response) => {
     const { username, password } = request.body;
